16507bc48364036c0e689284979d0e0b35e25a9d
simonZweimueller <simon.zweimueller1@gmail.com>
2020-11-01 10:34:25 +0100

    let the user accept room invitations

---
+++ roomix-frontend/src/app/roomlist/join-room/join-room.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { JoinRoomComponent } from './join-room.component';
+
+describe('JoinRoomComponent', () => {
+  let component: JoinRoomComponent;
+  let fixture: ComponentFixture<JoinRoomComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ JoinRoomComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(JoinRoomComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ roomix-frontend/src/app/roomlist/join-room/join-room.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/user';
+import {AccountService} from '../../account/account.service';
+import {FriendRequest} from '../../models/friend-request';
+import {first} from 'rxjs/operators';
+import {RoomInvite} from '../../models/room-invite';
+
+@Component({
+  selector: 'app-join-room',
+  templateUrl: './join-room.component.html',
+  styleUrls: ['./join-room.component.scss'],
+})
+export class JoinRoomComponent implements OnInit {
+
+  user: User;
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit() {
+    this.user = this.accountService.userValue;
+  }
+
+  response(roomInvite: RoomInvite, accept: boolean) {
+    this.accountService.roomInviteResponse(roomInvite, accept)
+        .pipe(first())
+        .subscribe(data => {
+          console.log(data);
+        });
+  }
+
+}
