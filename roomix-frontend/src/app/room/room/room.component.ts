1cedb0163bd8bf83939aeb574b7b6193008021cf
simonZweimueller <simon.zweimueller1@gmail.com>
2020-11-01 10:42:19 +0100

    songs automatically start and are synced to other users that are in the same room

--- roomix-frontend/src/app/room/room/room.component.ts
+++ roomix-frontend/src/app/room/room/room.component.ts
@@ -4,6 +4,11 @@ import {ActivatedRoute, Params, Router} from '@angular/router';
 import {RoomService} from '../room.service';
 import {first} from 'rxjs/operators';
 import {PlaylistService} from '../playlist/playlist.service';
+import {Song} from '../../models/song';
+import {AccountService} from '../../account/account.service';
+import {PlaySongService} from './play-song.service';
+import {DomSanitizer} from '@angular/platform-browser';
+import {User} from '../../models/user';
 
 @Component({
   selector: 'app-room',
@@ -12,15 +17,19 @@ import {PlaylistService} from '../playlist/playlist.service';
 })
 export class RoomComponent implements OnInit {
 
+  user: User;
   room: Room;
+  friendUserName: string;
 
   constructor(
       private playlistService: PlaylistService,
       private route: ActivatedRoute,
       private roomService: RoomService,
       private router: Router,
-  ) {
-  }
+      private accountService: AccountService,
+      private playSongService: PlaySongService,
+      private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -29,12 +38,30 @@ export class RoomComponent implements OnInit {
               .pipe(first())
               .subscribe(data => {
                 this.room = data;
+                console.log(this.room);
+                this.playSongService.connect(this.accountService.userValue.username, this.room.id);
+                console.log(this.playSongService.currentSong);
+                this.user = this.accountService.userValue;
               });
         }
     );
+    console.log(this.playSongService.currentSong);
+  }
+
+  sanitize(songUrl: string, songStart: number) {
+      console.log('https://www.youtube.com/embed/' + songUrl + '?start=' + songStart + '&controls=1&amp&autoplay=1');
+      return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + songUrl + '?start=' + songStart + '&controls=1&amp&autoplay=1');
   }
 
   showPlaylist() {
       this.router.navigate(['playlist', this.room.id], {relativeTo: this.route});
+  }
+
+  sendRoomInvite() {
+      this.accountService.sendRoomInvite(this.room.id, this.user.username, this.friendUserName)
+          .pipe(first())
+          .subscribe(data => {
+              console.log(data);
+          });
   }
 }
