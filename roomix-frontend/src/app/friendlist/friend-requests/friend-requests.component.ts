85111bacbe1495d304ffd4047cd09200d961b4ea
simonZweimueller <simon.zweimueller1@gmail.com>
2020-11-01 10:39:28 +0100

    handle friendRequest acceptations

---
+++ roomix-frontend/src/app/friendlist/friend-requests/friend-requests.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {AccountService} from '../../account/account.service';
+import {User} from '../../models/user';
+import {first} from 'rxjs/operators';
+import {FriendRequest} from '../../models/friend-request';
+
+@Component({
+  selector: 'app-friend-requests',
+  templateUrl: './friend-requests.component.html',
+  styleUrls: ['./friend-requests.component.scss'],
+})
+export class FriendRequestsComponent implements OnInit {
+
+  user: User;
+
+  constructor(
+      private accountService: AccountService,
+  ) { }
+
+  ngOnInit() {
+    this.user = this.accountService.userValue;
+  }
+
+  response(friendRequest: FriendRequest, accept: boolean) {
+    this.accountService.friendRequestResponse(friendRequest, accept)
+        .pipe(first())
+        .subscribe(data => {
+          console.log(data);
+        });
+  }
+
+}
