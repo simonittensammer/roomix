4c602524bb608c9eac696df8091fca70a0a63b75
simonittensammer <simon.ittensammer@gmx.at>
2020-11-06 21:35:26 +0100

    add volume input-range (wip)

--- roomix-frontend/src/app/control-bar/control-bar.component.ts
+++ roomix-frontend/src/app/control-bar/control-bar.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-control-bar',
-  templateUrl: './control-bar.component.html',
-  styleUrls: ['./control-bar.component.scss'],
+    selector: 'app-control-bar',
+    templateUrl: './control-bar.component.html',
+    styleUrls: ['./control-bar.component.scss'],
 })
 export class ControlBarComponent implements OnInit {
 
-  constructor() { }
+    volumePercentage = 100;
 
-  ngOnInit() {}
+    constructor() {
+    }
 
+    ngOnInit() {
+    }
+
+    adjustVolume(value) {
+        this.volumePercentage = value;
+        // console.log(this.volumePercentage);
+
+        // tslint:disable-next-line:triple-equals
+        if (this.volumePercentage == 0) {
+            console.log('mute');
+        } else if (this.volumePercentage <= 33) {
+            console.log('low');
+        } else if (this.volumePercentage <= 66) {
+            console.log('medium');
+        } else {
+            console.log('high');
+        }
+    }
 }
