d899760a85d24514cb18eba88e878818c1c9ff0e
simonittensammer <simon.ittensammer@gmx.at>
2020-12-05 16:33:01 +0100

    add current-room navigation functionality

--- roomix-frontend/src/app/header/header.component.ts
+++ roomix-frontend/src/app/header/header.component.ts
@@ -3,6 +3,8 @@ import {AccountService} from '../account/account.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Observable} from 'rxjs';
 import {User} from '../models/user';
+import {RoomService} from "../room/room.service";
+import {Room} from "../models/room";
 
 @Component({
   selector: 'app-header',
@@ -13,9 +15,11 @@ export class HeaderComponent implements OnInit {
 
   isLoggedIn: Observable<boolean>;
   user: User;
+  room: Room;
 
   constructor(
       private accountService: AccountService,
+      private roomService: RoomService,
       private router: Router,
       private route: ActivatedRoute
   ) {}
@@ -23,6 +27,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     this.isLoggedIn = this.accountService.isLoggedIn;
     this.user = this.accountService.userValue;
+    this.room = this.roomService.roomValue;
   }
 
   logout() {
