754c39136a6f0f24127c5a26c3ec0f49dfd8e69a
lorenzgruber <regru.lg@gmail.com>
2021-02-09 11:35:22 +0100

    use SocketMessageDTO to receive data from the socket

--- roomix-frontend/src/app/services/play-song.service.ts
+++ roomix-frontend/src/app/services/play-song.service.ts
@@ -5,13 +5,15 @@ import {webSocket} from 'rxjs/webSocket';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import {RoomService} from './room.service';
 import {Room} from '../models/room';
+import {SocketMessageDTO} from '../models/dto/SocketMessageDTO';
+import {PlaySongMessageDTO} from '../models/dto/PlaySongMessageDTO';
 
 @Injectable({
     providedIn: 'root'
 })
 export class PlaySongService {
 
-    songSocket: WebSocketSubject<Song>;
+    songSocket: WebSocketSubject<SocketMessageDTO>;
     currentSong: Song;
     currentSongUrl: string;
     currentSongTimer: number;
@@ -37,35 +39,30 @@ export class PlaySongService {
         this.connected = true;
 
         this.songSocket.asObservable().subscribe(
-            data => {
+            data  => {
                 console.log(data);
-                // @ts-ignore
-                // console.log(data);
-                // @ts-ignore
+
                 if (data.type === 'new-song') {
-                    // @ts-ignore
-                    this.currentSong = data.message.song;
+                    const message: PlaySongMessageDTO = data.message as PlaySongMessageDTO;
+                    this.currentSong = message.song;
                     this.currentSongUrl = this.currentSong.url;
-                    // @ts-ignore
-                    this.currentSongTimer = data.message.time;
+                    this.currentSongTimer = message.time;
                     this.completeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/'
                         + this.currentSongUrl + '?start=' + this.currentSongTimer + '&controls=1&amp&autoplay=1');
                 }
-                // @ts-ignore
+
                 else if (data.type === 'add-song') {
-                    // @ts-ignore
-                    const song = JSON.parse(data.message);
+                    const song: Song = data.message as Song;
                     this.room.playlist.songList.push(song);
                 }
-                // @ts-ignore
+
                 else if (data.type === 'remove-song') {
-                    // @ts-ignore
-                    const song = JSON.parse(data.message);
+                    console.log('removeing');
+                    const song: Song = data.message as Song;
                     this.room.playlist.songList.splice(this.room.playlist.songList.findIndex(x => x.url === song.url), 1);
                 }
-                // @ts-ignore
+
                 else if (data.type === 'stop') {
-                    // @ts-ignore
                     console.log('stop playing song');
                     this.completeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(null);
 
