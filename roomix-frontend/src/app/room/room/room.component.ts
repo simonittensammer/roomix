1b2f0c6443c435fc6b82d130f29ebe3ead781bd8
simonZweimueller <simon.zweimueller1@gmail.com>
2021-02-10 19:47:53 +0100

    seperate between join and connect

--- roomix-frontend/src/app/room/room/room.component.ts
+++ roomix-frontend/src/app/room/room/room.component.ts
@@ -65,4 +65,30 @@ export class RoomComponent implements OnInit {
     collapseList() {
         this.collapsed = !this.collapsed;
     }
+
+    userIsMember() {
+        for (const member of this.room.memberList) {
+            if (member.user.id === this.user.id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    joinRoomFunc() {
+        console.log('Joined Room');
+        this.roomService.addMember(this.user.username, this.room.id).subscribe(
+            value => {
+                console.log(value);
+                /*this.accountService.getProperMemberList(this.user.username).subscribe(value2 => {
+                    this.user.memberList = value2;
+                    this.accountService.updateUserValue(this.user);
+                });*/
+            });
+    }
+
+    leaveRoomFunc() {
+        // ADD HTTP REQUEST ONCE ENDPOINT IS CREATED
+        console.log('Left Room');
+    }
 }
