bb7b9e4a201f91da2143c847fd1c1b8c2f393715
simonZweimueller <simon.zweimueller1@gmail.com>
2021-02-10 15:57:18 +0100

    disable current Room button if not connected to any room

--- roomix-frontend/src/app/header/header.component.ts
+++ roomix-frontend/src/app/header/header.component.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import {User} from '../models/user';
 import {RoomService} from '../services/room.service';
 import {Room} from '../models/room';
+import {PlaySongService} from '../services/play-song.service';
 
 @Component({
   selector: 'app-header',
@@ -18,13 +19,13 @@ export class HeaderComponent implements OnInit {
 
   constructor(
       private accountService: AccountService,
-      private roomService: RoomService,
+      private playSongService: PlaySongService,
       private router: Router,
       private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    this.roomService.roomValue.subscribe(
+    this.playSongService.roomValue.subscribe(
         value => {
           this.room = value;
         }
