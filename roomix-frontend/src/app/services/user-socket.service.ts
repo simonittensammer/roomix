708d83ea65d547e6f608566e340ae2ae283edf4d
simonZweimueller <simon.zweimueller1@gmail.com>
2021-02-11 16:28:36 +0100

    update friendrequests and roominvites with a websocket

--- roomix-frontend/src/app/services/user-socket.service.ts
+++ roomix-frontend/src/app/services/user-socket.service.ts
@@ -3,6 +3,11 @@ import {WebSocketSubject} from 'rxjs/internal-compatibility';
 import {SocketMessageDTO} from '../models/dto/SocketMessageDTO';
 import {webSocket} from 'rxjs/webSocket';
 import {AccountService} from './account.service';
+import {UserService} from './user.service';
+import {first} from 'rxjs/operators';
+import {User} from '../models/user';
+import {Room} from '../models/room';
+import {RoomService} from './room.service';
 
 @Injectable({
     providedIn: 'root'
@@ -10,30 +15,68 @@ import {AccountService} from './account.service';
 export class UserSocketService {
 
     userSocket: WebSocketSubject<SocketMessageDTO>;
+    user: User;
+    room: Room;
 
-    constructor() {
+    constructor(
+        private userService: UserService,
+        private roomService: RoomService
+    ) {
     }
 
-
     connect(username) {
-        this.userSocket = webSocket('ws://localhost:8080/user/' + username);
+        this.userService.userValue.subscribe(
+            value => {
+                this.user = value;
+            });
+        this.roomService.roomValue.subscribe(
+            value => {
+                this.room = value;
+            });
 
+        this.userSocket = webSocket('ws://localhost:8080/user/' + username);
+        console.log(username);
         this.userSocket.asObservable().subscribe(data => {
 
             if (data.type === 'receive-friend-request') {
                 console.log('receive-friend-request');
+                this.userService.getProperFriendRequestList(username)
+                    .pipe(first())
+                    .subscribe( value => {
+                        this.user.friendRequestList = value;
+                        this.userService.updateUserValue(this.user);
+                    });
             }
 
             if (data.type === 'friend-request-response') {
                 console.log('friend-request-response');
+                this.userService.getProperFriendList(username)
+                    .pipe(first())
+                    .subscribe( value => {
+                        this.user.friendList = value;
+                        this.userService.updateUserValue(this.user);
+                    });
             }
 
             if (data.type === 'receive-room-invite') {
                 console.log('receive-room-invite');
+                this.userService.getProperRoomInviteList(username)
+                    .pipe(first())
+                    .subscribe( value => {
+                        this.user.roomInviteList = value;
+                        this.userService.updateUserValue(this.user);
+                    });
             }
 
-            if (data.type === 'room-invite-response') {
-                console.log('room-invite-response');
+            if (data.type === 'receive-room-invite') {
+                console.log('receive-room-invite');
+                console.log(data.message);
+                /*this.roomService.getMembers(data.message)
+                    .pipe(first())
+                    .subscribe( value => {
+                        this.room.memberList = value;
+                        this.roomService.updateRoomValue(this.room);
+                    });*/
             }
         });
     }
