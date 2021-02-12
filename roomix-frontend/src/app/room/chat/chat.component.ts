9d2acb4b7b300548feffeb156fdf39e247c19b1b
lorenzgruber <regru.lg@gmail.com>
2021-02-12 11:00:46 +0100

    implement chat component

---
+++ roomix-frontend/src/app/room/chat/chat.component.ts
@@ -0,0 +1,56 @@
+import {Component, OnInit} from '@angular/core';
+import {ChatMessageDTO} from "../../models/dto/chatMessageDTO";
+import {RoomService} from "../../services/room.service";
+import {Room} from "../../models/room";
+import {User} from "../../models/user";
+import {UserService} from "../../services/user.service";
+import {UserSocketService} from "../../services/user-socket.service";
+import {PlaySongService} from "../../services/play-song.service";
+
+@Component({
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.scss'],
+})
+export class ChatComponent implements OnInit {
+
+    room: Room;
+    user: User;
+    messages: ChatMessageDTO[];
+
+    message: string;
+
+    constructor(
+        private roomService: RoomService,
+        private userService: UserService,
+        private playSongService: PlaySongService
+    ) {
+    }
+
+    ngOnInit() {
+        this.roomService.roomValue.subscribe(
+            room => {
+                this.room = room;
+            });
+
+        this.userService.userValue.subscribe(
+            value => {
+                this.user = value;
+            });
+    }
+
+    sendMessage() {
+        this.playSongService.sendChatMessage(this.message);
+    }
+
+    userIsMember() {
+        for (const member of this.room.memberList) {
+            if (member.user.id === this.user.id) {
+                console.log('is member');
+                return true;
+            }
+        }
+        console.log('is not member');
+        return false;
+    }
+}
