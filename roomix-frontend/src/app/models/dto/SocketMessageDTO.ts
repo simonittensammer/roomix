c88a4f51b7d802f108b111e9cdca2530b3334832
lorenzgruber <regru.lg@gmail.com>
2021-02-12 10:59:38 +0100

    change SocketMessageDTO to class instead of interface

--- roomix-frontend/src/app/models/dto/SocketMessageDTO.ts
+++ roomix-frontend/src/app/models/dto/SocketMessageDTO.ts
@@ -1,4 +1,9 @@
-export interface SocketMessageDTO {
+export class SocketMessageDTO {
     type: string;
     message: object;
+
+    constructor(type: string, message: object) {
+        this.type = type;
+        this.message = message;
+    }
 }
