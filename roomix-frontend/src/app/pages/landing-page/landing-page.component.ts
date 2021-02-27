003ffce72a01c57135241198f13bca6b37eec8d5
simonittensammer <simon.ittensammer@gmx.at>
2021-02-27 20:15:02 +0100

    add parallax scrolling in landing-page banner

--- roomix-frontend/src/app/pages/landing-page/landing-page.component.ts
+++ roomix-frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  @ViewChild('scrollBox') private scrollBox: ElementRef;
+  scrollPx: number;
 
   constructor() { }
 
   ngOnInit() {}
 
+  onScroll($event: Event) {
+    this.scrollPx = this.scrollBox.nativeElement.scrollTop;
+  }
 }
