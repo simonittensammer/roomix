1ec5dbf694e8e958a2b8da90beda21f9bf569b62
lorenz <regru.lg@gmail.com>
2021-03-14 09:03:00 +0100

    add import.sql script with default room tags

---
+++ roomix-backend/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tag ("tag_name") VALUES ('Pop');
+INSERT INTO tag ("tag_name") VALUES ('Rock');
+INSERT INTO tag ("tag_name") VALUES ('Metal');
+INSERT INTO tag ("tag_name") VALUES ('Jazz');
+INSERT INTO tag ("tag_name") VALUES ('Hip Hop');
+INSERT INTO tag ("tag_name") VALUES ('EDM');
+INSERT INTO tag ("tag_name") VALUES ('Country');
+INSERT INTO tag ("tag_name") VALUES ('Blues');
+INSERT INTO tag ("tag_name") VALUES ('Classical');
